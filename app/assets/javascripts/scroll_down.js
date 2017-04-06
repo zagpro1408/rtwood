7ab9eb4e1979a42b341bd9ac4d97d4a7f19613b5
zagpro1408 <zagorskybobuyc@gmail.com>
2017-04-06 13:04:19 +0300

    Fix button - leave request

--- app/assets/javascripts/scroll_down.js
+++ app/assets/javascripts/scroll_down.js
@@ -1,5 +1,6 @@
+// Ссылки в nav
 $(document).ready(function(){
-	$("#menu").on("click","a", function (event) {
+	$("#scroll_down_id").on("click","a", function (event) {
 		//отменяем стандартную обработку нажатия по ссылке
 		event.preventDefault();
 
@@ -10,6 +11,23 @@ $(document).ready(function(){
 			top = $(id).offset().top;
 
 		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
+
+// Кнопка "ОСТАВИТЬ ЗАЯВКУ" в разделе О Компании
+$(document).ready(function(){
+	$("#scroll_down_about_us").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 500);
 	});
 });

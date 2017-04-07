8eec7e303d71c5586092f9f2118c7448d181f143
zagpro1408 <zagorskybobuyc@gmail.com>
2017-04-07 11:12:25 +0300

    Convert a app from sqlite to MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20170405053727) do
 
-  create_table "orders", force: :cascade do |t|
+  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "company"
     t.string   "name"
     t.string   "email"
     t.string   "number"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end

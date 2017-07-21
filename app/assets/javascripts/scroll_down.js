9b42c0bcbb376367420228d42906d65cfc082d3d
uladzimir-zahorski <Zahorski@1pt.com>
2017-07-21 14:07:51 +0300

    add transparent for navbar

--- app/assets/javascripts/scroll_down.js
+++ app/assets/javascripts/scroll_down.js
@@ -31,3 +31,12 @@ $(document).on('turbolinks:load', function(){
 		$('body,html').animate({scrollTop: top}, 600);
 	});
 });
+
+$(document).scroll(function() {
+if ($(this).scrollTop() > 1){
+    $('.navbar').addClass("sticky");
+  }
+  else{
+    $('.navbar').removeClass("sticky");
+  }
+});
